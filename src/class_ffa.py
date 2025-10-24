9f6d68dce3ed767abb0ccbd957394761a92e8db0
Nicolas Lima <nicolas.lima.tech@gmail.com>
2025-10-24 08:46:22 -0300

    Adição da modelagem revisada do FFA (Formal Feature Attribution) - formal_ffa.py

--- src/class_ffa.py
+++
@@ -1,5 +0,0 @@
-class FFA:
-    def __init__(self, model, features, n_feat):
-        self.model = model
-        self.features = features
-        self.n_feat = n_feat
--- src/xgbooster/__init__.py
+++
@@ -1,4 +0,0 @@
-from .encode import *
-from .tree import *
-from .xgbooster import *
-from .preprocess import *
